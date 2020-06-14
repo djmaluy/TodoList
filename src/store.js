970922cdee4ffde95e5a99b9d3c08eb770030125
Andrii Tsvirko <a.tsvirko@ukr.net>
2020-06-14 20:03:49 +0200

    test commit

--- src/store.js
+++ src/store.js
@@ -1,18 +1,9 @@
-import React from "react";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import logger from "redux-logger";
 
-function todos(state = [], action) {
-  switch (action.type) {
-    case "ADD_TODO":
-      return state.concat([action.text]);
-    default:
-      return state;
-  }
-}
+import rootReducer from "./reducers";
 
-const store = createStore(todos, ["Use Redux"]);
-
-store.dispatch({
-  type: "ADD_TODO",
-  text: "Read the docs",
-});
+export default () => {
+  const store = createStore(rootReducer, applyMiddleware(logger));
+  return store;
+};
